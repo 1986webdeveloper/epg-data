8cfe07b7344bee68cc1c3316a70d84111facb994
1986webdeveloper <1986webdeveloper@gmail.com>
2017-01-23 13:10:52 +0530

    add epg function and template file

---
+++ tpl-epg.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Template Name: EPG
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fourteen
+ * @since Twenty Fourteen 1.0
+ */
+
+error_reporting(0);
+get_header();
+
+//Display EPG data.
+readEPG();
+
+get_footer();
+
+?>
